f2d3c587fa4ea01a76e1e64b17dec290ec0dec70
Dikachi J <dikachimichael253@gmail.com>
2024-10-18 05:41:58 +0100

    created task_6.sql script

---
+++ task_6.sql
@@ -0,0 +1,1 @@
+INSERT INTO customer (customer_id, customer_name, email, address ) VALUES ('2','Blessing Malik','bmalik@sandtech.com','123 Happiness Ave.'),('3','Obed Ehoneah','eobed@sandtech.com','123 Happiness Ave.'),('4','Nehemial Kamolu','nkamolu@sandtech.com','123 Happiness Ave.');

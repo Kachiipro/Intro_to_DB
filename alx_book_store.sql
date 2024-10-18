562526c0fd906e1dc6f8383cce952e057dff67ab
Dikachi J <dikachimichael253@gmail.com>
2024-10-18 03:48:32 +0100

    created sql  alx_book_store.sql script

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -16,4 +16,4 @@ CREATE TABLE Customers (customer_id Primary Key, customer_name VARCHAR(215),emai
 
 CREATE TABLE Orders (order_id Primary Key, customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE);
 
-CREATE TABLE Order_Details (orderdetailid INT Primary Key, order_id INT, FOREIGN KEY(order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE);
+CREATE TABLE Order_Details (orderdetailid INT Primary Key, order_id INT, FOREIGN KEY (order_id) REFERENCES Orders(order_id), book_id INT, FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE);
